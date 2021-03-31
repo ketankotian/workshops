b5f532e032ed62277f7a31492d93a9cba75f4415
ketankotian <krkkotian@gmail.ocm>
2021-03-31 14:05:54 +0530

    created uc3

--- src/workshop/TicTacToe.java
+++ src/workshop/TicTacToe.java
@@ -10,22 +10,31 @@ public class TicTacToe {
 	public static void main(String[] args) {
 		boardInitialization();
 		selectMark();
+		showBoard(board);
 	}
 
 	// board initialization
 	static void boardInitialization() {
-
 		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 	}
 
-	// selection x or o
+	// selection of x or o
 	public static void selectMark() {
 		char mark;
 		System.out.println("Choose symbol X or O");
 		mark = sc.next().charAt(0);
 
+	}
+
+	// displaying board
+	static void showBoard(char[] board) {
+		System.out.println("| " + board[1] + " | " + board[2] + " | " + board[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[4] + " | " + board[5] + " | " + board[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + board[7] + " | " + board[8] + " | " + board[9] + " |");
 	}
 
 }
